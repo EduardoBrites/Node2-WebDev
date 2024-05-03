765a164c5169fe5fe726964a046e8e4afd0161a4
Logon Aluno <logonrmlocal@fiap.com.br>
2024-05-03 09:49:00 -0300

    Processos iniciais para a criação do projeto

---
+++ src/Components/Header/Header.jsx
@@ -0,0 +1,15 @@
+import Navbar from "../Navbar/Navbar";
+
+function Header() {
+    return ( 
+        <>
+        <header>
+            <span>Olá, visitante!</span>
+            <h1>Bem-Vindo ao CineApp</h1>
+            <Navbar/>
+        </header>
+        </>
+    );
+}
+
+export default Header;
